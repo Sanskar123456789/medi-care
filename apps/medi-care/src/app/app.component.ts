085de04bd3052c9a78b103cc9725520b34731418
Sanskar123456789 <sanskar.agarwal91@gmail.com>
2022-07-24 12:05:36 +0530

    initial commit

---
+++ apps/medi-care/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'medi-care-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'medi-care';
+}
